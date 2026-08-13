cec28c12ab6c9f8e16eee2c6c52439e599ef0c00
Soumya <nalinisahoo781@gmail.com>
2026-08-13 22:23:06 +0530

    Fixed url mismatches

--- server/socket.js
+++ server/socket.js
@@ -40,7 +40,7 @@ export const initializeSocket = (server) => {
 
   io = new Server(server, {
     cors: {
-      origin: "http://localhost:5173",
+      origin: process.env.FRONTEND_URL,
       credentials: true,
     },
   });
